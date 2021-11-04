f34c78cf2d1c6272791c99c882f62ba16066296b
Alex Gabriel Oyola <gabriel_oyola@outlook.com>
2021-11-04 14:00:36 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿
+namespace C.A.D.A_SOCIOS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btAgregar = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDNI = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.cbCarnet = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbEstado = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtBuscador = new System.Windows.Forms.TextBox();
+            this.btBuscar = new System.Windows.Forms.Button();
+            this.dg = new System.Windows.Forms.DataGridView();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.btEliminar = new System.Windows.Forms.Button();
+            this.labds = new System.Windows.Forms.Label();
+            this.cbPlatea = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btAgregar
+            // 
+            this.btAgregar.Location = new System.Drawing.Point(581, 272);
+            this.btAgregar.Name = "btAgregar";
+            this.btAgregar.Size = new System.Drawing.Size(137, 31);
+            this.btAgregar.TabIndex = 0;
+            this.btAgregar.Text = "AGREGAR";
+            this.btAgregar.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(-57, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(469, 353);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(361, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "SISTEMA DE SOCIOS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(357, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "NOMBRE:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(360, 165);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(198, 20);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(590, 165);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(198, 20);
+            this.txtApellido.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(587, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "APELLIDO:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(357, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 18);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "DOCUMENTO:";
+            // 
+            // txtDNI
+            // 
+            this.txtDNI.Location = new System.Drawing.Point(360, 219);
+            this.txtDNI.Name = "txtDNI";
+            this.txtDNI.Size = new System.Drawing.Size(198, 20);
+            this.txtDNI.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(587, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 18);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "EDAD:";
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(590, 219);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(79, 20);
+            this.txtEdad.TabIndex = 10;
+            // 
+            // cbCarnet
+            // 
+            this.cbCarnet.FormattingEnabled = true;
+            this.cbCarnet.Location = new System.Drawing.Point(675, 219);
+            this.cbCarnet.Name = "cbCarnet";
+            this.cbCarnet.Size = new System.Drawing.Size(117, 21);
+            this.cbCarnet.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(675, 198);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 18);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "CARNET:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(359, 257);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 18);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "ESTADO:";
+            // 
+            // cbEstado
+            // 
+            this.cbEstado.FormattingEnabled = true;
+            this.cbEstado.Location = new System.Drawing.Point(360, 278);
+            this.cbEstado.Name = "cbEstado";
+            this.cbEstado.Size = new System.Drawing.Size(198, 21);
+            this.cbEstado.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(357, 71);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(277, 18);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "INGRESE EL NUMERO DE SOCIO:";
+            // 
+            // txtBuscador
+            // 
+            this.txtBuscador.Location = new System.Drawing.Point(360, 92);
+            this.txtBuscador.Name = "txtBuscador";
+            this.txtBuscador.Size = new System.Drawing.Size(171, 20);
+            this.txtBuscador.TabIndex = 16;
+            // 
+            // btBuscar
+            // 
+            this.btBuscar.Location = new System.Drawing.Point(541, 92);
+            this.btBuscar.Name = "btBuscar";
+            this.btBuscar.Size = new System.Drawing.Size(138, 20);
+            this.btBuscar.TabIndex = 18;
+            this.btBuscar.Text = "BUSCAR";
+            this.btBuscar.UseVisualStyleBackColor = true;
+            // 
+            // dg
+            // 
+            this.dg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg.Location = new System.Drawing.Point(12, 325);
+            this.dg.Name = "dg";
+            this.dg.Size = new System.Drawing.Size(883, 280);
+            this.dg.TabIndex = 19;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(354, 115);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(843, 20);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "---------------------------------------------------------------------------------" +
+    "----------------------------------------------------------";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(354, 302);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(843, 20);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "---------------------------------------------------------------------------------" +
+    "----------------------------------------------------------";
+            // 
+            // btEliminar
+            // 
+            this.btEliminar.Location = new System.Drawing.Point(724, 272);
+            this.btEliminar.Name = "btEliminar";
+            this.btEliminar.Size = new System.Drawing.Size(137, 31);
+            this.btEliminar.TabIndex = 22;
+            this.btEliminar.Text = "Eliminar";
+            this.btEliminar.UseVisualStyleBackColor = true;
+            // 
+            // labds
+            // 
+            this.labds.AutoSize = true;
+            this.labds.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labds.Location = new System.Drawing.Point(798, 197);
+            this.labds.Name = "labds";
+            this.labds.Size = new System.Drawing.Size(74, 18);
+            this.labds.TabIndex = 24;
+            this.labds.Text = "PLATEA:";
+            // 
+            // cbPlatea
+            // 
+            this.cbPlatea.FormattingEnabled = true;
+            this.cbPlatea.Location = new System.Drawing.Point(798, 218);
+            this.cbPlatea.Name = "cbPlatea";
+            this.cbPlatea.Size = new System.Drawing.Size(107, 21);
+            this.cbPlatea.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(907, 617);
+            this.Controls.Add(this.labds);
+            this.Controls.Add(this.cbPlatea);
+            this.Controls.Add(this.btEliminar);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.dg);
+            this.Controls.Add(this.btBuscar);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtBuscador);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cbEstado);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cbCarnet);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDNI);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btAgregar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btAgregar;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDNI;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.ComboBox cbCarnet;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cbEstado;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtBuscador;
+        private System.Windows.Forms.Button btBuscar;
+        private System.Windows.Forms.DataGridView dg;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button btEliminar;
+        private System.Windows.Forms.Label labds;
+        private System.Windows.Forms.ComboBox cbPlatea;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C.A.D.A_SOCIOS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbCarnet.Items.Add("Niño/a-$750");
+            cbCarnet.Items.Add("Juvenil-$900");
+            cbCarnet.Items.Add("Adulto-$1200");
+
+            cbPlatea.Items.Add("Norte");
+            cbPlatea.Items.Add("Sur");
+            cbPlatea.Items.Add("Este");
+            cbPlatea.Items.Add("Oeste");
+
+            cbEstado.Items.Add("Activo");
+            cbEstado.Items.Add("Inactivo");
+
+
+        }
+    }
+}
